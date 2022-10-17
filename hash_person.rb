948bd310ed18d2194946d55069d6c356055423f9
gsista-booth <gsista@chicagobooth.edu>
2022-10-17 01:31:04 +0000

    hash person

--- hash_person.rb
+++ hash_person.rb
@@ -2,3 +2,19 @@
 # Make sure the value of the :age key is an Integer
 
 p "Enter a name, age, and occupation separated by spaces:"
+
+person1 = gets.chomp.split(" ")
+
+name = person1.at(0)
+age = person1.at(1).to_i
+occupation = person1.at(2)
+
+p name, age, occupation
+
+person2 = Hash.new
+
+person2.store(:name, name)
+person2.store(:age, age)
+person2.store(:occupation, occupation)
+
+p person2
